bdb94fe03112793fdeb8ae45aa2083a32e0ae72c
DmitriyVik <62546488+DmitriyVik@users.noreply.github.com>
2020-05-05 14:44:49 +0300

    JSlesson1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 const cartButton = document.querySelector("#cart-button");
-const close = document.querySelector(".close");
 const modal = document.querySelector(".modal");
-// console.log(cartButton);
-// cartButton.addEventListener("click", function (event) {
-//   modal.classList.add("is-open");
-// });
-// close.addEventListener("click", function (event) {
-//   modal.classList.remove("is-open");
-// });
+const close = document.querySelector(".close");
+
 cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
 
@@ -15,4 +9,71 @@ function toggleModal() {
   modal.classList.toggle("is-open");
 }
 
-new WOW().init();
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closelAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+
+let login = localStorage.getItem("Delivery");
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function autorized() {
+  console.log("авторизован");
+  function logOut(event) {
+    login = null;
+    localStorage.removeItem("Delivery");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+    checkAuth();
+  }
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+
+  buttonOut.addEventListener("click", logOut);
+}
+
+function notAutorized() {
+  console.log(login);
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+    localStorage.setItem("Delivery", login);
+
+    buttonAuth.removeEventListener("click", toggleModalAuth);
+    closelAuth.removeEventListener("click", toggleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+
+    logInForm.reset();
+    if (login == "") {
+      alert("Введите пароль");
+    } else {
+      toggleModalAuth();
+    }
+    checkAuth();
+  }
+
+  console.log("no авторизован");
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closelAuth.addEventListener("click", toggleModalAuth);
+
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    autorized();
+  } else {
+    notAutorized();
+  }
+}
+checkAuth();
